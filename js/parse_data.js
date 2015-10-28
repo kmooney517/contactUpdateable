6da5699dcb52194938abc040a6d32cce630f1591
Kevin Mooney <kmooney517@gmail.com>
2015-10-28 13:05:18 -0400

    initial commit

---
+++ js/parse_data.js
@@ -0,0 +1,9 @@
+const APP_ID = 'ARxR2a8JJDiWXa0mhX4988s4FmB94N2L8a4QVpco';
+const API_KEY = 'HxtA5WyFyekwmYdZ4lCiizKaTXtIlceHpILqT9Ti';
+const APP_URL = 'https://api.parse.com/1/classes/contacts';
+
+export {
+  APP_ID,
+  API_KEY,
+  APP_URL
+};
